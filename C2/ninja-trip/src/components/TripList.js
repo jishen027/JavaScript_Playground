ee67546898267d005d699132fd033ff04a3fd353
J Li <jli306@sheffield.ac.uk>
2022-05-28 22:49:54 +0100

    [ADD] shuffleCards

--- C2/ninja-trip/src/components/TripList.js
+++ C2/ninja-trip/src/components/TripList.js
@@ -4,12 +4,13 @@ import './TripList.css'
 
 export default function TripList() {
   const [url, setUrl] = useState('http://localhost:3001/trips')
-  const { data: trips, isPending} = useFetch(url)
+  const { data: trips, isPending, error } = useFetch(url)
 
   return (
     <div className="trip-list">
       <p>Trip list for show all the trips in snapshot</p>
       {isPending && <div>Loading trips</div>}
+      {error && <div>{error}</div>}
       <ul>
         {trips && trips.map((trip, index) => (
           <li key={trip.id}>
